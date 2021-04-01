d007a832dedc1536d8c02d1c69dc91ed9343ce0b
Jarod Waggener <jarod2228@rocketmail.com>
2021-03-31 21:08:46 -0500

    almost done with lesson 2 exercise

--- lesson1/exercises/src/app/app.module.ts
+++ lesson1/exercises/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { ChoresListComponent } from './chores-list/chores-list.component';
 import { FavPhotosComponent } from './fav-photos/fav-photos.component';
+import { MoviePageComponent } from './movie-page/movie-page.component';
+import { FavLinksComponent } from './fav-links/fav-links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
     ChoresListComponent,
-    FavPhotosComponent
+    FavPhotosComponent,
+    MoviePageComponent,
+    FavLinksComponent
   ],
   imports: [
     BrowserModule
