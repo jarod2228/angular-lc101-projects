2549b69f05322faf641a3764891e795e51e54580
Jarod Waggener <jarod2228@rocketmail.com>
2021-04-08 11:50:46 -0500

    got as far as I could with experimentation without help of TF's, they all seem to be busy helping people with last-minute assignment work

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,13 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  confirmFlight() {
+    let confirmed = window.confirm("Confirm that the shuttle is ready for takeoff.");
+    if (confirmed) {
+      document.getElementById("flightStatusHTML").innerHTML = "Shuttle in flight";
+      // document.getElementsByClassName("shuttle-background").style.color = "blue";
+      //shis is bullhonky ^^^ that one, pick up here
+    }
+  }
 }
